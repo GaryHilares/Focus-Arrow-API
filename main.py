0ff3a4ff75bfe1107859c9811750d629ce4092a3
Gary Hilares <dev.garyhilares@gmail.com>
2024-07-15 13:12:12 -0500

    Updated location of app for compatibility with Vercel

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from liberty_arrow.app import create_app
 from dotenv import load_dotenv
 
+load_dotenv(".env")
+app = create_app()
+
 if __name__ == "__main__":
-    load_dotenv(".env")
-    app = create_app()
     app.run()
